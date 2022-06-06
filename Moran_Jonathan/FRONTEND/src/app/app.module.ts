b8acdc6072f36a0eb67ce86bc95f973eb3174ee5
Jonathan <jonathan.moran9348@gmail.com>
2022-06-06 20:31:41 -0300

    Nueva actualizacion de Front

--- Moran_Jonathan/FRONTEND/src/app/app.module.ts
+++ Moran_Jonathan/FRONTEND/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HysComponent } from './hys/hys.component';
 import { ProyectoComponent } from './proyecto/proyecto.component';
 import { FooterComponent } from './footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 
 
@@ -28,7 +30,9 @@ import { HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HysComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
